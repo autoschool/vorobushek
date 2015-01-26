5fd10a24220e7c1d9995b763250f0983a49414a5
Sidelnikov Yurii <sidelnikov.yurii@gmail.com>
2015-01-26 12:32:01 +0300

    redesign for proector

--- js/sequencejs-options.modern-slide-in.js
+++ js/sequencejs-options.modern-slide-in.js
@@ -2,17 +2,11 @@ $(document).ready(function(){
     var options = {
         nextButton: true,
         prevButton: true,
-        pagination: true,
+        pagination: false,
         animateStartingFrameIn: true,
-        autoPlay: true,
+        autoPlay: false,
         autoPlayDelay: 3000,
-        preloader: true,
-        preloadTheseFrames: [1],
-        preloadTheseImages: [
-            "images/tn-model1.png",
-            "images/tn-model2.png",
-            "images/tn-model3.png"
-        ]
+        preloader: false
     };
     
     var mySequence = $("#sequence").sequence(options).data("sequence");
